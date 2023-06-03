277a8f2a1a53be47c183adf7d119e82abe0a2714
Confidence Madiope <confidencem97@gmail.com>
2023-06-03 15:41:04 +0200

    finished

---
+++ app/about/page.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "About next 13 app dir",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Next-13 App dir</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, magni iusto
+        numquam, dicta velit sed tempore deleniti suscipit dolore quam ea odit.
+        Vero quibusdam itaque, tempore quam iure dolorum animi commodi sit
+        corporis velit, molestias sapiente! Ducimus at, neque officiis sit
+        laudantium adipisci deserunt rem nisi corporis, accusantium, quaerat
+        expedita.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Out Team</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum libero
+        dolor minus laudantium nemo temporibus voluptate dolores voluptatibus
+        saepe culpa.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
